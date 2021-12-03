54691bc0f9de42b88de76b9f1b79a9dd1ceb42ce
josiah tan <josiah123t@gmail.com>
2021-12-03 18:36:48 +1000

    added swap and move

--- lua/josiah/treesitter.lua
+++ lua/josiah/treesitter.lua
@@ -43,6 +43,34 @@ require'nvim-treesitter.configs'.setup {
         ["ib"] = "@block.inner",
       },
     },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>sl"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>sh"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
   },
 }
-
