a97f3d8f33b4091b8e41bae45eac5989f606282c
josiah tan <josiah123t@gmail.com>
2022-01-01 21:08:54 +1100

    bug fix
    if there is no entry for table 2, then return t1
    this bug is produced when there is no entry for the language that is
    being queried

--- lua/josiah/cht_sh.lua
+++ lua/josiah/cht_sh.lua
@@ -43,8 +43,10 @@ local function selectQuery(prompt_bufnr)
 end
 
 local function tableConcat(t1,t2)
-	for i=1,#t2 do
-		t1[#t1+1] = t2[i]  --corrected bug. if t1[#t1+i] is used, indices will be skipped
+	if t2 then
+		for i=1,#t2 do
+			t1[#t1+1] = t2[i]  --corrected bug. if t1[#t1+i] is used, indices will be skipped
+		end
 	end
 	return t1
 end
