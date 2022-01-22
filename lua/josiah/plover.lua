889bf9c55f885ed75730ca70ffa40814c2ce3427
josiah tan <josiah123t@gmail.com>
2022-01-23 02:34:47 +1100

    updated plover config

--- lua/josiah/plover.lua
+++ lua/josiah/plover.lua
@@ -1,6 +1,12 @@
 -- for plover-live-view-nvim
+
 require("plover_viewer").setup({
 	builtin = {
+		viewer = {
+			terminal = {
+				command_args = {"-f", "---disable-inotify"}
+			}
+		},
 		cwd = {"~/.config/plover/", "/mnt/c/Users/josia/AppData/Local/plover/plover/"},
 		file_name = "clippy.txt"
 	},
@@ -8,6 +14,22 @@ require("plover_viewer").setup({
 })
 
 
-vim.keymap.set('n', '<leader>pc', function () RELOAD("plover_viewer.builtin").view() end)
-vim.keymap.set('n', '<leader>pv', function () RELOAD("plover_viewer.builtin").splitToggle() end)
-vim.keymap.set('n', '<leader>pp', function () RELOAD("plover_viewer.builtin").view({file_name = "tapey_tape.txt"}) end)
+vim.keymap.set('n', '<leader>pp', function () RELOAD("plover_viewer.builtin").view() end)
+vim.keymap.set('n', '<leader>ps', function () RELOAD("plover_viewer.builtin").splitToggle({
+	viewer = {
+		terminal = {
+			number = 6
+		}
+	}
+}) end)
+vim.keymap.set('n', '<leader>pv', function () RELOAD("plover_viewer.builtin").splitToggle({
+	file_name = "tapey_tape.txt",
+	viewer = {
+		terminal = {
+			number = 7
+		}
+	},
+	split = {
+		choose = "vertical"
+	}
+}) end)
