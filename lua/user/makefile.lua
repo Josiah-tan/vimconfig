0d87e1be484d89cd0370f3b1b35cab7a5ef103d1
Josiah-tan <josiah123t@gmail.com>
2022-08-30 21:39:00 +1000

    added cmake build automation

--- lua/user/makefile.lua
+++ lua/user/makefile.lua
@@ -14,4 +14,19 @@ vim.api.nvim_set_keymap("n", "<Leader>ms", ':lua require("makefile_nvim.make").r
 vim.api.nvim_set_keymap("n", "<Leader>txf", ':lua require("makefile_nvim.tox").run({term = 1})<CR>', {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("n", "<Leader>txt", ':lua require("makefile_nvim.tox").run({term = 1, selection = "test"})<CR>', {noremap = true, silent = true, expr = false})
 
+-- defined in makefile.lua
+local CMakeFile = function(opts)
+	vim.cmd[[:wa]]
+	build_directory = vim.fn.getcwd().."/build"
+	if vim.fn.isdirectory(build_directory) then
+		require("harpoon.term").sendCommand(opts.term, "cd "..build_directory.."\n")
+		require("harpoon.term").sendCommand(opts.term, "cmake ..\n")
+		require("harpoon.term").sendCommand(opts.term, "cmake  --build .\n")
+	end
+	require("harpoon.term").gotoTerminal(opts.term)
+end
+
+vim.keymap.set("n", "<leader>cmf", function () CMakeFile({term = 1}) end)
+-- vim.keymap.set("n", "<leader>cmf", function () require("harpoon.term").sendCommand(1, [[cd]]) end))
+-- vim.api.
 return M
