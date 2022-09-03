1d3f0a0a758f58cb0f3c24826bea16f2a95e0ed4
Josiah-tan <josiah123t@gmail.com>
2022-09-03 16:40:47 +1000

    added compile_commands.json generator from makefile

--- lua/user/makefile.lua
+++ lua/user/makefile.lua
@@ -10,14 +10,24 @@ vim.api.nvim_set_keymap("n", "<Leader>mc", ':lua require("makefile_nvim.make").r
 vim.api.nvim_set_keymap("n", "<Leader>md", ':lua require("makefile_nvim.make").run({term = 1, selection = "docs"})<CR>', {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("n", "<Leader>ms", ':lua require("makefile_nvim.make").run({term = 1, selection = "stress"})<CR>', {noremap = true, silent = true, expr = false})
 
+vim.keymap.set("n", "<Leader>mr", function () require("harpoon.term").sendCommand(1,
+[[make --always-make --dry-run | grep -wE 'gcc|g\+\+|c\+\+' | grep -w '\-c' | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]' > compile_commands.json]] .. "\n"
+ ) end)
+-- [[make --always-make --dry-run \   
+--  | grep -wE 'gcc|g\+\+|c\+\+' \
+--  | grep -w '\-c' \
+--  | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]' \
+--  >u compile_commands.json]]
+
 
 vim.api.nvim_set_keymap("n", "<Leader>txf", ':lua require("makefile_nvim.tox").run({term = 1})<CR>', {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("n", "<Leader>txt", ':lua require("makefile_nvim.tox").run({term = 1, selection = "test"})<CR>', {noremap = true, silent = true, expr = false})
 
+
 -- defined in makefile.lua
 local CMakeFile = function(opts)
 	vim.cmd[[:wa]]
-	build_directory = vim.fn.getcwd().."/build"
+	local build_directory = vim.fn.getcwd().."/build"
 	if vim.fn.isdirectory(build_directory) then
 		require("harpoon.term").sendCommand(opts.term, "cd "..build_directory.."\n")
 		require("harpoon.term").sendCommand(opts.term, "cmake ..\n")
