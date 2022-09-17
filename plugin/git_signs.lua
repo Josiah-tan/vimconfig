8e9ac337ebefafe821d64bdb3af06009b70ece9a
Josiah Tan <jtan7828@uni.sydney.edu.au>
2022-09-17 20:52:33 +1000

    formatting

--- plugin/git_signs.lua
+++ plugin/git_signs.lua
@@ -1,40 +1,40 @@
 require('gitsigns').setup{
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
+		-- Navigation
+		map('n', ']c', function()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function() gs.next_hunk() end)
+			return '<Ignore>'
+		end, {expr=true})
 
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
+		map('n', '[c', function()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function() gs.prev_hunk() end)
+			return '<Ignore>'
+		end, {expr=true})
 
-    -- Actions
-    map({'n', 'v'}, '<leader>ghs', ':Gitsigns stage_hunk<CR>')
-    map({'n', 'v'}, '<leader>ghr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>ghS', gs.stage_buffer)
-    map('n', '<leader>ghu', gs.undo_stage_hunk)
-    map('n', '<leader>ghR', gs.reset_buffer)
-    map('n', '<leader>ghp', gs.preview_hunk)
-    map('n', '<leader>ghb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>gtb', gs.toggle_current_line_blame)
-    map('n', '<leader>ghd', gs.diffthis)
-    map('n', '<leader>ghD', function() gs.diffthis('~') end)
-    map('n', '<leader>gtd', gs.toggle_deleted)
+		-- Actions
+		map({'n', 'v'}, '<leader>ghs', ':Gitsigns stage_hunk<CR>')
+		map({'n', 'v'}, '<leader>ghr', ':Gitsigns reset_hunk<CR>')
+		map('n', '<leader>ghS', gs.stage_buffer)
+		map('n', '<leader>ghu', gs.undo_stage_hunk)
+		map('n', '<leader>ghR', gs.reset_buffer)
+		map('n', '<leader>ghp', gs.preview_hunk)
+		map('n', '<leader>ghb', function() gs.blame_line{full=true} end)
+		map('n', '<leader>gtb', gs.toggle_current_line_blame)
+		map('n', '<leader>ghd', gs.diffthis)
+		map('n', '<leader>ghD', function() gs.diffthis('~') end)
+		map('n', '<leader>gtd', gs.toggle_deleted)
 
-    -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
+		-- Text object
+		map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+	end
 }
