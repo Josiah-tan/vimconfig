dd7ec5be8ec78a6dc112856d57c1184bd971c125
josiah <josiah123t@gmail.com>
2022-12-13 15:43:35 +1100

    cpp template less stuff in it

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -8,20 +8,10 @@
 using namespace std;
 
 int main(void){
-	// helloWorld();
-	int T, N;
+	int T;
 	cin >> T;
-	for (int t = 0; t < T;t ++){
-		cin >> N;
-		vector<int>v = vector<int>(N);
-		for (int i = 0; i < N; i++){
-			cin >> v[i];
-		}
-		std::sort(v.begin(), v.end());
-		for (int i = 0; i < N; i++){
-			cout << v[i] << ' ';
-		}
-		cout << endl;
+	for (int t = 0; t < T; t++){
+		
 	}
 }
 
