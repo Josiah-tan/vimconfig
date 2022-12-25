e20c9e8ad46e16a1ca162fc9a9689c5e2e371e6e
josiah <josiah123t@gmail.com>
2022-12-25 21:39:15 +1100

    changed Cartesian and added point

--- templates/python/competitive.py
+++ templates/python/competitive.py
@@ -1,22 +1,61 @@
 class Point:
+    def __init__(self, y: int, x: int):
+        self.y = y
+        self.x = x
+
+    def int(self):
+        return Point(int(self.y), int(self.x))
+
+    def __repr__(self) -> str:
+        return(f"P{self.y, self.x}")
+
+    def __add__(self, obj):
+        return Cartesian(self.y + obj.y, self.x + obj.x)
+    
+    def __sub__(self, obj):
+        return Cartesian(self.y - obj.y, self.x - obj.x)
+        
+    def __abs__(self):
+        return Cartesian(abs(self.y), abs(self.x))
+        
+    def distanceManhattan(self, obj):
+        distance = abs(self - obj)
+        return distance.x + distance.y
+
+    def __eq__(self, obj):
+        return self.x == obj.x and self.y == obj.y
+    
+    def __hash__(self):
+        return hash((self.y, self.x))
+
+    def __rmul__(self, obj):
+        return self.__mul__(obj)
+
+    def __mul__(self, obj):
+        if type(obj) == int:
+            return Point(self.y * obj, self.x * obj)
+        else:
+            return NotImplementedError
+
+class Cartesian:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def int(self):
-        return Point(int(self.x), int(self.y))
+        return Cartesian(int(self.x), int(self.y))
 
     def __repr__(self) -> str:
         return(f"P{self.x, self.y}")
 
     def __add__(self, obj):
-        return Point(self.x + obj.x, self.y + obj.y)
+        return Cartesian(self.x + obj.x, self.y + obj.y)
     
     def __sub__(self, obj):
-        return Point(self.x - obj.x, self.y - obj.y)
+        return Cartesian(self.x - obj.x, self.y - obj.y)
         
     def __abs__(self):
-        return Point(abs(self.x), abs(self.y))
+        return Cartesian(abs(self.x), abs(self.y))
         
     def distanceManhattan(self, obj):
         distance = abs(self - obj)
@@ -33,12 +72,12 @@ class Point:
 
     def __mul__(self, obj):
         if type(obj) == int:
-            return Point(self.x * obj, self.y * obj)
+            return Cartesian(self.x * obj, self.y * obj)
         else:
             return NotImplementedError
 
 class Line:
-    def __init__(self, point: Point, gradient) -> None:
+    def __init__(self, point: Cartesian, gradient) -> None:
         self.gradient = gradient
         self.intercept = point.y - gradient * point.x
     
@@ -48,7 +87,7 @@ class Line:
         else:
             x = (obj.intercept - self.intercept) / (self.gradient - obj.gradient)
             y = self.gradient * x + self.intercept
-        return Point(x, y)
+        return Cartesian(x, y)
 
     def __repr__(self):
         return f"(m, y) = ({self.gradient}, {self.intercept})"
--- templates/python/main.py
+++ templates/python/main.py
@@ -1,6 +1,6 @@
 file_number = 1
 part_1 = True
-# from personal import Point, sign
+# from competitive import Point, sign
 # import numpy as np
 with open(f'{file_number}.txt') as f:
     entries = f.read().rstrip().split('\n')
