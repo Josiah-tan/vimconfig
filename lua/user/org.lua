c6b90502352b3971c5d2e453dbad30cf8ba96ce7
josiah <josiah123t@gmail.com>
2024-04-01 12:23:14 +1100

    captioning without tmux

--- lua/user/org.lua
+++ lua/user/org.lua
@@ -13,14 +13,19 @@ end
 M.captionSplit = function()
 	-- vim.fn.system([[tmux split-window -v -p 20 "nvim -c 'call CustomLog()' -c 'set laststatus=0' .; $SHELL"]])  -- finally something that actually works
 	if vim.fn.isdirectory(vim.fn.expand(folder)) == 1 then
-		local not_marked = vim.fn.system("tmux display -p -t '~' '#D'") == "no marked target\n"
-		if not_marked then
-			vim.fn.system([[tmux split-window -v -p 20 "tmux select-pane -m && nvim -c 'call CustomLog()' -c 'set laststatus=0' ."]])  -- finally something that actually works
-		else
-			local tried = vim.fn.system([[tmux join-pane -p 20 && tmux select-pane -m]])
-			if tried == "can't join a pane to its own window\n" then
-				vim.fn.system([[tmux select-pane -t '~']])
+		if vim.fn.getenv("TMUX") ~= vim.NIL then
+			local not_marked = vim.fn.system("tmux display -p -t '~' '#D'") == "no marked target\n"
+			if not_marked then
+				vim.fn.system([[tmux split-window -v -p 20 "tmux select-pane -m && nvim -c 'call CustomLog()' -c 'set laststatus=0' ."]])  -- finally something that actually works
+			else
+				local tried = vim.fn.system([[tmux join-pane -p 20 && tmux select-pane -m]])
+				if tried == "can't join a pane to its own window\n" then
+					vim.fn.system([[tmux select-pane -t '~']])
+				end
 			end
+		else
+			vim.cmd("belowright 10 split")
+			M.customLog()
 		end
 	else
 		print("clone or stow (todo) your org files first man")
