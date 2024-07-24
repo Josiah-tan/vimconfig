b790df64d3294561d041fb292fab216c477daf8f
Josiah-tan <josiah123t@gmail.com>
2024-07-24 22:58:31 +1000

    ooh looks good • → ◼︎

--- after/plugin/css.lua
+++ after/plugin/css.lua
@@ -1,1 +1,19 @@
-require("tailwind-tools").setup({})
+require("tailwind-tools").setup(
+	{
+		document_color = {
+			enabled = true,
+			kind = "inline",
+			inline_symbol = "◼︎", -- Example of a simpler symbol
+			debounce = 200,
+		},
+		conceal = {
+			enabled = false,
+			min_length = nil,
+			symbol = "◼︎", -- Example of a simpler symbol
+			highlight = {
+				fg = "#38BDF8",
+			},
+		},
+		custom_filetypes = {}
+	}
+)
